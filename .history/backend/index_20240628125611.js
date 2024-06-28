459b0a96e28531f1371b85208034820bd095eaf7
Jaime Ng <jaimengjm@gmail.com>
2024-06-28 12:58:24 +0800

    Fix attempt for cors

--- .history/backend/index_20240628125611.js
+++ .history/backend/index_20240628125611.js
@@ -41,7 +41,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://ros-mon.vercel.app");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   next();
 });
